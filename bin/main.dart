647a58780fd5463957b7db4453563d644e3686b7
Manuel de Jesús Gámez López <winmanuel09@gmail.com>
2022-05-15 11:54:48 -0600

    tabla Dart

---
+++ bin/main.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int tabla = 80;
+  var dato = "Hola Dart de Google";
+  for (int i = 1; i < 11; i++) {
+    //print('hello ${i + 1}');
+    print('${tabla} * ${i} = ${tabla * i}');
+    //print(dato);
+  }
+
+  print('Fin del buble for');
+}
---
+++ test/tablamultiplicar_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tablamultiplicar_dart/tablamultiplicar_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
